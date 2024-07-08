d9144b290875fc7fca971af1a4f0b5dbf51463bf
neobugs1 <n.smileski@gmail.com>
2024-07-09 00:24:46 +0200

    manage npm run build to include particle config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,23 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "particlesjs-config.json",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
