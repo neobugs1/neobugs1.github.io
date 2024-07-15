25644ad3b698ee50e0fa6c5030fa43b27fa1104b
neobugs1 <n.smileski@gmail.com>
2024-07-15 12:39:46 +0200

    fix scroll position, lang fix, center header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="flex justify-between items-center p-4 bg-gray-800 dark:bg-gray-950 text-white">
     <LanguagePicker />
-    <h1 class="text-xl">{{ $t("welcome.name") }}</h1>
+    <h1 class="text-xl fixed left-1/2 -translate-x-1/2">{{ $t("welcome.name") }}</h1>
     <DarkModeToggle />
   </header>
 </template>
--- src/components/LanguagePicker.vue
+++ src/components/LanguagePicker.vue
@@ -1,34 +1,62 @@
 <template>
-  <div class="flex justify-center items-center">
-    <a
+  <div class="flex justify-start items-center w-1/12">
+    <button
       :class="{ 'text-gray-500 cursor-not-allowed': locale === 'en' }"
       @click="changeLanguage('en')"
       :disabled="locale === 'en'"
-      href="#"
       class="md:px-1 py-1 hover:text-gray-600 text-sm transition duration-200 ease-in-out"
-      >EN</a
     >
+      EN
+    </button>
     <span class="mx-1.5 text-lg">|</span>
-    <a
+    <button
       :class="{ 'text-gray-500 cursor-not-allowed': locale === 'mk' }"
       @click="changeLanguage('mk')"
       :disabled="locale === 'mk'"
-      href="#"
       class="md:px-1 py-1 hover:text-gray-600 text-sm transition duration-200 ease-in-out"
-      >MK</a
     >
+      MK
+    </button>
   </div>
 </template>
 
 <script>
 import { useI18n } from "vue-i18n";
+import { nextTick, onMounted, onUnmounted } from "vue";
 
 export default {
   setup() {
     const { locale } = useI18n();
-    const changeLanguage = (lang) => {
-      locale.value = lang;
+    let scrollPosition = 0;
+    let isChangingLanguage = false;
+
+    const disableScroll = (event) => {
+      if (isChangingLanguage) {
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+      }
     };
+
+    const changeLanguage = async (lang) => {
+      scrollPosition = window.scrollY;
+      isChangingLanguage = true;
+      locale.value = lang;
+      await nextTick();
+      setTimeout(() => {
+        window.scrollTo({ top: scrollPosition, behavior: "smooth" });
+        isChangingLanguage = false;
+      }, 0);
+    };
+
+    onMounted(() => {
+      window.addEventListener("scroll", disableScroll, { passive: false });
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("scroll", disableScroll);
+    });
+
     return { locale, changeLanguage };
   },
 };
